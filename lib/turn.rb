efc3b4956e14e5c43b738d6ef685af8bc893ad32
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-13 00:06:33 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your #valid_move? method here
+def valid_move?(board,index)
+  if position_taken?(board, index) and index.between?(0,8)
+    true
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  if board[index] == " " or board[index] == "" or board[index] == nil
+    !false
+  elsif board[index] == "X" or board[index] == "O"
+    !true
+  end
+end
+
+
